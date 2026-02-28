ddbfb6a4c368d4c4f32156a31f981fafc5176643
princesojitra66 <princesojitra3914@gmail.com>
2026-02-28 13:01:33 +0530

    interview exam day1

--- prince-exam-day1/models/adminModel.js
+++ prince-exam-day1/models/adminModel.js
@@ -4,9 +4,11 @@ const adminSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    confirm_password: { type: String, required: true },
     status: { type: Boolean, default: true },
     created_date: { type: Date, default: Date.now },
     updated_date: { type: Date, default: Date.now }
 });
+
 
 module.exports = mongoose.model('Admin', adminSchema);
